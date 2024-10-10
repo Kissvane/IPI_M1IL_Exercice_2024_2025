149f853a1c1514b3e32d257d5ed0131f2e3034d3
Kissvane <adrien@nebulae.world>
2024-10-10 16:26:14 +0200

    Exercice 9 to 10

---
+++ Exo10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPI_C_sharp_2024_202
+{
+    public class Exo10
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Tapez un nombre");
+            int value = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            for (int i = 0; i <= value; i++)
+            {
+                result += i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Exo9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPI_C_sharp_2024_202
+{
+    public class Exo9
+    {
+        public void Execute()
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public void Execute2()
+        {
+            for (int i = 0; i <= 20; i = i+2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void Execute3()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i*2);
+            }
+        }
+    }
+}
